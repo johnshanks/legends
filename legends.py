ce1e566bb82417699c5f03d946a97f5b2a44ce4e
johnshanks <johnshanks@gmail.com>
2016-03-07 21:34:30 -0600

    Objects, NPCs
    Generalization section of rogue walkthrough.

--- legends.py
+++ legends.py
@@ -8,6 +8,29 @@ LIMIT_FPS = 20 #20 frames per second maximum
 #Sets speed of real time game. Remove for turn based.
 #libtcod.sys_set_fps(LIMIT_FPS)
 
+class Object:
+	#this is a generic object: player, monster, item, stairs...
+	#always represented by a character on screen
+	def __init__(self, x, y, char, color):
+		self.x = x
+		self.y = y
+		self.char = char
+		self.color = color
+		
+	def move(self, dx, dy):
+		#move by given amount
+		self.x += dx
+		self.y += dy
+		
+	def draw(self):
+		#set color and draw character for this object at position
+		libtcod.console_set_default_foreground(con, self.color)
+		libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+		
+	def clear(self):
+		#erase the character that represents this object
+		libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
 #Monitor for key presses
 def handle_keys():
 	global playerx, playery
@@ -24,13 +47,13 @@ def handle_keys():
 	
 	#movement keys
 	if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-		playery -= 1
+		player.move(0, -1)
 	elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-		playery += 1
+		player.move(0, 1)
 	elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-		playerx -= 1
+		player.move(-1, 0)
 	elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-		playerx += 1
+		player.move(1, 0)
 
 #############################################
 #Main game loop. Runs as long as window open
@@ -42,16 +65,31 @@ libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | 
 #Init window. TrueFalse param toggle full screen
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
 
-#Set player to center of screen
-playerx = SCREEN_WIDTH/2
-playery = SCREEN_HEIGHT/2
+#Console
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+#player
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+#npc
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+
+#objects on screen
+objects = [npc, player]
 
 while not libtcod.console_is_window_closed():
-	libtcod.console_set_default_foreground(0, libtcod.white)
-	libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
+	
+	#draw all objects on screen
+	for object in objects:
+		object.draw()
+
+	#libtcod.console_set_default_foreground(con, libtcod.white)
+	#libtcod.console_put_char(con, playerx, playery, '@', libtcod.BKGND_NONE)
+	libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 	libtcod.console_flush()
 	
-	libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
+	#erase all objects at old location before move
+	for object in objects:
+		object.clear()
 	
 	#handle keys and exit game
 	exit = handle_keys()
